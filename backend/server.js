73277339163d5ff83b8c351f8c5e28880139b982
Chernikovmax <chernik.max@gmail.com>
2019-09-01 17:42:01 +0300

    [DEL] some unnecessary files and cleaned up the code.

--- backend/server.js
+++ backend/server.js
@@ -28,19 +28,3 @@ server.listen(3030, err => {
   if (err) throw err;
   console.log("listening on port 3030");
 });
-
-// const WebSocket = require("ws");
-
-// const wsServer = new WebSocket.Server({ port: 8080 });
-
-// // const clients = new Set();
-
-// wsServer.on("connection", ws => {
-//   ws.on("message", incomingData => {
-//     wsServer.clients.forEach(client => {
-//       if (client !== ws && client.readyState === WebSocket.OPEN) {
-//         client.send(incomingData);
-//       }
-//     });
-//   });
-// });
