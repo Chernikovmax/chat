4a6b0aae4c874a62cc4eb58db967a97168c2d297
Chernikovmax <chernik.max@gmail.com>
2019-09-03 14:59:10 +0300

    [MOD] Redone server part. Now it has just a method to receive/send messages and loggers.

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,22 @@
-const server = require("http").createServer();
+const app = require("express")();
+const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
+const PORT = 3030;
+
+server.listen(PORT, err => {
+  if (err) throw err;
+  console.log("Listening on port 3030");
+});
+
 io.on("connection", client => {
-  client.on("register", handleRegister);
-
-  client.on("join", handleJoin);
-
-  client.on("leave", handleLeave);
-
-  client.on("message", handleMessage);
-
-  client.on("chatrooms", handleGetChatrooms);
-
-  client.on("availableUsers", handleGetAvailableUsers);
+  console.log("user connected");
 
   client.on("disconnect", () => {
     console.log(`User "${client.id}" was disconnected...`);
-    handleDisconnect();
   });
 
-  client.on("error", err => {
-    console.error(`Received error from user "${client.id}":`, err);
+  client.on("message", data => {
+    io.emit("message", data);
   });
-});
-
-server.listen(3030, err => {
-  if (err) throw err;
-  console.log("listening on port 3030");
 });
